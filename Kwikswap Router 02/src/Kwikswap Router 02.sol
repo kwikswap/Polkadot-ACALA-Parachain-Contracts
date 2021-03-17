6b5f2157cccd2f8fbb771fb92000962a30b5437b
KwikSwap <73940078+kwikswap@users.noreply.github.com>
2021-03-17 16:13:53 +0500

    Update Kwikswap Router 02.sol

--- Kwikswap Router 02/src/Kwikswap Router 02.sol
+++ Kwikswap Router 02/src/Kwikswap Router 02.sol
@@ -69,12 +69,7 @@ library TransferHelper {
 library KwikswapV1Library {
     using SafeMath for uint;
 
-    // returns sorted token addresses, used to handle return values from pairs sorted in this order
-    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
-        require(tokenA != tokenB, 'KwikswapV1Library: IDENTICAL_ADDRESSES');
-        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), 'KwikswapV1Library: ZERO_ADDRESS');
-    }
+
 
     // calculates the CREATE2 address for a pair without making any external calls
     function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
@@ -94,12 +89,6 @@ library KwikswapV1Library {
         (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
     }
 
-    // given some amount of an asset and pair reserves, returns an equivalent amount of the other asset
-    function quote(uint amountA, uint reserveA, uint reserveB) internal pure returns (uint amountB) {
-        require(amountA > 0, 'KwikswapV1Library: INSUFFICIENT_AMOUNT');
-        require(reserveA > 0 && reserveB > 0, 'KwikswapV1Library: INSUFFICIENT_LIQUIDITY');
-        amountB = amountA.mul(reserveB) / reserveA;
-    }
 
     // given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
     function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
