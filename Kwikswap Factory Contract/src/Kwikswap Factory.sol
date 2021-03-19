4f052547396c104a8ab7f43770d4d897ec1eba29
KwikSwap <73940078+kwikswap@users.noreply.github.com>
2021-03-20 00:55:27 +0500

    Update Kwikswap Factory.sol

--- Kwikswap Factory Contract/src/Kwikswap Factory.sol
+++ Kwikswap Factory Contract/src/Kwikswap Factory.sol
@@ -148,7 +148,7 @@ interface IKwikswapV1Pair {
         uint amount1In,
         uint amount0Out,
         uint amount1Out,
-        address indexed to
+        
     );
     event Sync(uint112 reserve0, uint112 reserve1);
 
@@ -157,8 +157,7 @@ interface IKwikswapV1Pair {
     function token0() external view returns (address);
     function token1() external view returns (address);
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
-    function price0CumulativeLast() external view returns (uint);
-    function price1CumulativeLast() external view returns (uint);
+    
     function kLast() external view returns (uint);
 
     function mint(address to) external returns (uint liquidity);
